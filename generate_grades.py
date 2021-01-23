6c70951cec4350dc21e5af3492265894a4cc2b1a
Marek Marecki <marekjm@ozro.pw>
2021-01-23 10:33:00 +0100

    WIP

--- generate_grades.py
+++ generate_grades.py
@@ -2,6 +2,7 @@ import os
 import sys
 import hashlib
 import json
+import datetime
 
 
 def main(args):
@@ -13,16 +14,22 @@ def main(args):
         grades_source = json.loads(ifstream.read())
 
     meetings = grades_source['meetings']
-    print('students: {}'.format(len(grades_source['students'].keys())))
+    students = grades_source['students']
+    print('students: {}'.format(len(students.keys())))
     print('meetings: {}'.format(', '.join(map(lambda _: _['date'], meetings))))
 
+    PASSING_GRADE = 3.0
+
     failing_students = []
-    for index, data in grades_source['students'].items():
+    for index, data in students.items():
         name = data['name']
         nick = (data['nick'] or None)
 
         gs = []
         for i, m in enumerate(meetings):
+            if datetime.datetime.strptime(m['date'], '%Y-%m-%d') > datetime.datetime.now():
+                continue
+
             grade = 2.0
             try:
                 grade = data['grades'][i]
@@ -36,7 +43,7 @@ def main(args):
 
         gs_useful = list(filter(lambda _: _[1], gs))
         average_grade = sum(map(lambda _: _[1], gs_useful)) / len(gs_useful)
-        if average_grade < 3:
+        if average_grade < PASSING_GRADE:
             failing_students.append((index, name, nick, average_grade,))
 
         print('  s{}: {} ({}): {:4.2f}'.format(
@@ -60,14 +67,18 @@ def main(args):
             ofstream.write('|       Data | Ocena\n')
             ofstream.write('+------------+------\n')
             for date, grade in gs:
-                ofstream.write('| {} | {:4.2f} ({})\n'.format(
+                ofstream.write('| {} | {:4.2f}  ({})\n'.format(
                     date['date'],
                     grade,
                     date['comment'],
                 ))
             ofstream.write('+------------+------\n')
 
-    print('failing: {}'.format(len(failing_students)))
+    print('failing: {} of {} ({:5.2f}%)'.format(
+        len(failing_students),
+        len(students.keys()),
+        ((len(failing_students) / len(students.keys())) * 100.0),
+    ))
     for index, name, nick, average_grade in failing_students:
         print('  s{}: {} ({}): {:4.2f}'.format(index, name, nick,
             average_grade))
