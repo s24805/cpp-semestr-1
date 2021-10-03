4a0ab491d6f0ad16df942ac667dee3b15392d382
Pawel Pisarski <ppisarski@pjwstk.edu.pl>
2021-10-03 16:42:50 +0200

    clean

--- src/00-hello-world.cpp
+++ src/00-hello-world.cpp
@@ -4,27 +4,11 @@
 
 
 #include <iostream>
-#include <string>
 
-auto zapytaj() -> int
-{
-    int x;
-std::string s = "Podaj liczbe: ";
-    std::cout << s;
-    std::cin >> x;
-    return x;
-}
 
-// komentarz changed
 auto main() -> int
 {
-    std::cout << "Hello, AAAA!\n";
-
-    int a, b;
-
-    a = zapytaj();
-    b = zapytaj();
-    std::cout << "suma: " << a+b << "\n";
+    std::cout << "Hello, World\n";
 
     return 0;
 }
--- src/my.cpp
+++ src/my.cpp
@@ -3,18 +3,17 @@
 
 auto iloczyn(int a, int b, int c) -> int
 {
-return a * b * c;
+    return a * b * c;
 }
 
 auto main() -> int
 {
+    int a, b, c;
+    std::cout << "Podaj liczbe: ";
+    std::cin >> a;
 
-int a, b, c;
-std::cout << "Podaj liczbe: ";
-std::cin >> a;
+    b = 1;
+    c = 2;
 
-b = 1;
-c = 2;
-
-std::cout << "Wynik: " << iloczyn(a, b, c);
+    std::cout << "Wynik: " << iloczyn(a, b, c);
 }
