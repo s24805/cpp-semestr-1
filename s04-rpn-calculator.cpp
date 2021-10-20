853d045d88df8d2df733f489ce1eeed0a592925f
s24805 <92035355+s24805@users.noreply.github.com>
2021-10-20 17:37:18 +0200

    Add files via upload

---
+++ s04-rpn-calculator.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+// ja nie czytam slajdow;
+{
+float a,b;
+float c;
+float e;
+int k,x,o,h;
+string j;
+cout<<"podaj dzialanie (*,/,//,%,**,sqrt,odwr)"<<endl;
+cin>>j;
+if(j=="*")
+{
+cout<<"podaj dwie liczby"<<endl;
+cin>>a;
+cin>>b;
+c=a*b;
+cout<<c;}
+if(j=="/")
+{
+cout<<"podaj dwie liczby"<<endl;
+cin>>a;
+cin>>b;
+c=a/b;
+cout<<c;}
+if(j=="//")
+{
+cout<<"podaj dwie liczby"<<endl;
+cin>>k;
+cin>>x;
+o=k%x;
+h=(k-o)/x;
+cout<<h;}
+if(j=="%")
+{
+cout<<"podaj dwie liczby"<<endl;
+cin>>k;
+cin>>x;
+h=k%x;
+cout<<h;
+}
+if(j=="**")
+{
+cout<<"podaj dwie liczby"<<endl;
+cin>>k;
+cin>>x;
+for(int i=0;i<x;i++)
+{
+h=h*k;
+}
+cout<<h;
+}
+if(j=="sqrt")
+{
+cout<<"podaj liczbe"<<endl;
+cin>>a;
+e=sqrt(a);
+cout<<e;
+}
+if(j=="odwr")
+{
+cout<<"podaj liczbe"<<endl;
+cin>>a;
+cout<<"odwrotnoscia liczby a jest 1/"<<a;
+}
+return 69;}
