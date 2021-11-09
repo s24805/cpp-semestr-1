298ae346c7de4eac0213a56f397b7146b1e45f89
s24805 <92035355+s24805@users.noreply.github.com>
2021-11-09 14:59:16 +0100

    Add files via upload

---
+++ s04-ctor-dtor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct jd
+{
+    string a="cos";
+};
+int main()
+{
+  jd a1;
+  a1.a+=" destructed";
+  cout<<a1.a;
+    return 324;
+}
+
