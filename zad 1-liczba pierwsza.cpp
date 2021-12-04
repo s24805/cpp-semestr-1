b2c5bba35b34189daca41b6997436c93d4dbcd5a
s24805 <92035355+s24805@users.noreply.github.com>
2021-12-04 11:09:19 +0100

    Add files via upload

---
+++ zad 1-liczba pierwsza.cpp
@@ -0,0 +1,36 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main()
+{
+    float pier;
+    int pet=0,x,y=1;
+    cout<<"podaj liczbe"<<endl;
+    cin>>x;
+    pier=sqrt(x);
+    int T[x]={0};
+    for(int i=2;i<=pier;i++)
+    {
+        if(T[i]==1)
+            continue;
+        if(x%i==0)
+        {
+            cout<<"najmniejszym dzielnikiem jest:   "<<i;
+            return 121;
+        }
+        if(x%i!=0)
+        {
+            for(int j=i;j<=pier;j=j*y)
+            {
+                T[j]=1;
+                y++;
+                pet++;
+            }
+        }
+        y=1;
+        pet++;
+    }
+    cout<<"liczba jest pierwsza"<<endl;
+    cout<<"wykonano w "<<pet<<" petlach";
+    return 123123;
+}
